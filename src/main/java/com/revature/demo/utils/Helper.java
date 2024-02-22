95db6e2af5ca93efb287bb5a71f883db6c123134
RoryEiffe <roryeiffe8@gmail.com>
2024-02-22 16:34:42 -0500

    Added old project.

---
+++ src/main/java/com/revature/demo/utils/Helper.java
@@ -0,0 +1,11 @@
+package com.revature.demo.utils;
+
+public class Helper {
+    public int add3 (int x, int y, int z) {
+        return x + y + z;
+    }
+
+    public int add2(int x, int y) {
+      return x + y;
+    }
+}
